dd7b9b5512c549c074fa17a9d400402f410b8552
chaemina <chaemina82@naver.com>
2023-06-09 15:19:50 +0900

    feat : 요구사항 1. autofocus 구현
    mini-signup-form 의 javascript 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 // TODO: 이 곳에 정답 코드를 작성해주세요.
+
+//1. id focus
+//이벤트 : 페이지(window) 로드 시 
+//핸들러 : focus
+const idEl = document.getElementById('id')
+window.addEventListener('load', ()=> {
+  idEl.focus()
+});
+
